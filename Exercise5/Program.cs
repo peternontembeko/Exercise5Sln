c18eba6a34125ae8fe0ee93bc2b7b449f6a4efce
peternontembeko <peternontembeko22@gmail.com>
2022-08-02 14:05:54 +0200

    Add project files.

---
+++ Exercise5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Exercise5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double sum = 0;
+            Console.WriteLine("Enter 5 numbers");
+            for(int i = 1; i <=5; i++)
+            {
+                Console.WriteLine("");
+                double num = Convert.ToDouble(Console.ReadLine());
+                sum += num;
+            }
+            double avarage = sum / 5;
+            Console.WriteLine($"The average is:{avarage}");
+        }
+    }
+}
